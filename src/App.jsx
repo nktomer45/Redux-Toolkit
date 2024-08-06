2efd5a6861b5b5493ffc0d26aa1d63f2dadb7528
neeraj-readwhere <neeraj.kumar@mediologysoftware.com>
2024-08-06 12:28:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./components/AddTodos";
+import  Todos  from "./components/Todos";
+function App() {
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
